6b25fb4566e8232eb70f3ce360a16bdddbdf4747
Saul Feliciano <jmartinez@intenpro.com>
2020-05-28 17:26:44 -0400

    Initial commit

---
+++ src/test/java/saulwebavanzada/demo/InventarioDeEquipoApplicationTests.java
@@ -0,0 +1,13 @@
+package saulwebavanzada.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventarioDeEquipoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
